a905969a1f012948f07406177c03dd308ba1e052
elmo <elmonazareno@gmail.com>
2013-08-15 10:13:20 +0800

    reorganized accounts

--- client/rameses-enterprise-accounts/src/com/rameses/enterprise/accounts/RevenueItemPage.java
+++ client/rameses-enterprise-accounts/src/com/rameses/enterprise/accounts/RevenueItemPage.java
@@ -4,7 +4,7 @@
  * Created on February 27, 2011, 12:48 PM
  */
 
-package com.rameses.enterprise.treasury.master;
+package com.rameses.enterprise.accounts;
 
 
 import com.rameses.osiris2.themes.FormPage;
