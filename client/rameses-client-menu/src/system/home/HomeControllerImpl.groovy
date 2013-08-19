b50eb94419c4206eda622c2e935bbcee8b6a1bca
worgflores <worgflores@gmail.com>
2013-08-19 10:16:04 +0800

    updated the HomeControllerImpl.groovy

--- client/rameses-client-menu/src/system/home/HomeControllerImpl.groovy
+++ client/rameses-client-menu/src/system/home/HomeControllerImpl.groovy
@@ -2,18 +2,25 @@ package system.home;
 
 import com.rameses.rcp.common.*;
 import com.rameses.rcp.annotations.*;
+import com.rameses.rcp.framework.*;
 import com.rameses.util.*;
 import com.rameses.osiris2.*;
 import com.rameses.osiris2.client.*;
 
 public class HomeControllerImpl {
 
-    def formActions;
+    def formActions = [];
     
     void init(){
-        formActions = InvokerUtil.lookupActions("home.action");
-        formActions?.each{
+        
+        def actionProvider = ClientContext.currentContext.actionProvider;
+        formActions = actionProvider.lookupActions('home.action');
+        formActions.each{ 
             if (!it.icon) it.icon = 'images/home-icon.png';
+
+            def target = it.properties.target+'';
+            if (!target.matches('window|popup|process|_window|_popup|_process')) 
+            	it.properties.target = 'window'; 
         }
     }
 
