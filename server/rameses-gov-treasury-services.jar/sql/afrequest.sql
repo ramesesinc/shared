d3ef52d4e364c6d69d2b80682136494d4d92610d
elmo <elmonazareno@gmail.com>
2013-09-17 19:28:56 +0800

    removed

--- server/rameses-gov-treasury-services.jar/sql/afrequest.sql
+++
@@ -1,7 +0,0 @@
-[getItems]
-SELECT ar.*, (ar.qty - ar.qtyreceived) as qtybalance,
-af.aftype, af.unit, af.unitqty, af.description, 
-af.serieslength, af.denomination
-FROM afrequestitem ar
-INNER JOIN af af ON af.objid=ar.af 
-WHERE ar.reqid = $P{objid}
