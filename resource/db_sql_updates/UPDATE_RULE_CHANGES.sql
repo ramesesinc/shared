d927dd23ec5ea2829a02524d1ca2e989f81f1368
Jessie S. Zamora <jzamss@gmail.com>
2013-12-06 10:58:03 +0800

    Updates on UPDATE_RULE_CHANGES

--- resource/db_sql_updates/UPDATE_RULE_CHANGES.sql
+++ resource/db_sql_updates/UPDATE_RULE_CHANGES.sql
@@ -53,34 +53,52 @@ SET ANSI_PADDING OFF
 GO
 
 INSERT INTO sys_ruleset_fact (ruleset, rulefact)
-SELECT ruleset, objid from sys_rule_fact;
+SELECT ruleset, objid from sys_rule_fact
+go
 
 
 INSERT INTO sys_ruleset_actiondef (ruleset, actiondef)
-SELECT ruleset, objid from sys_rule_actiondef;
+SELECT ruleset, objid from sys_rule_actiondef
+go
 
 ALTER TABLE sys_ruleset_fact
 ADD CONSTRAINT fk_rulesetfact_rulefact 
 FOREIGN KEY (rulefact)
-REFERENCES sys_rule_fact (objid);
+REFERENCES sys_rule_fact (objid)
+go
 
 ALTER TABLE sys_ruleset_fact
 ADD CONSTRAINT fk_rulesetfact_ruleset 
 FOREIGN KEY (ruleset)
-REFERENCES sys_ruleset (name);
+REFERENCES sys_ruleset (name)
+go
 
 
 ALTER TABLE sys_ruleset_actiondef
 ADD CONSTRAINT fk_rulesetactiondef_actiondef 
 FOREIGN KEY (actiondef)
-REFERENCES sys_rule_actiondef (objid);
+REFERENCES sys_rule_actiondef (objid)
+go
 
 ALTER TABLE sys_ruleset_actiondef
 ADD CONSTRAINT fk_rulesetactiondef_ruleset 
 FOREIGN KEY (ruleset)
-REFERENCES sys_ruleset (name);
+REFERENCES sys_ruleset (name)
+go
 
-ALTER TABLE sys_rule_fact DROP COLUMN ruleset;
-ALTER TABLE sys_rule_actiondef DROP COLUMN ruleset;
+alter table sys_rule_fact drop constraint DF__sys_rule___rules__707E9C7C
+go
+alter table sys_rule_fact drop constraint uid_ruleset_fact
+go
+ALTER TABLE sys_rule_fact DROP COLUMN ruleset
+go
+
+alter table sys_rule_actiondef drop constraint DF__sys_rule___rules__4B4D17CD
+go
+alter table sys_rule_actiondef drop constraint uid_ruleset_actiondef
+go
+ALTER TABLE sys_rule_actiondef DROP COLUMN ruleset
+go
+
 
 
