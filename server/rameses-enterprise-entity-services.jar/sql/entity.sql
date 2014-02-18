bac1ad9be3428375df1556a4e77eac03232b4d4e
elmo <elmonazareno@gmail.com>
2014-02-18 17:51:26 +0800

    edited entity

--- server/rameses-enterprise-entity-services.jar/sql/entity.sql
+++ server/rameses-enterprise-entity-services.jar/sql/entity.sql
@@ -1,41 +1,14 @@
 [getList]
 SELECT e.objid, e.entityno, e.name, e.address, e.type 
 FROM entity e 
-WHERE e.entityname LIKE $P{name}  
+WHERE e.entityname LIKE $P{searchtext}  
 ORDER BY e.entityname 
 
 [getLookup]
 SELECT e.objid, e.entityno, e.name, e.address, e.type 
 FROM entity e 
-WHERE e.entityname LIKE $P{name} and type IN ('INDIVIDUAL','JURIDICAL') 
-ORDER BY e.entityname 
-
-[getLookupIndividual]
-SELECT e.objid, e.entityno, e.name, e.address, e.type 
-FROM entity e 
-INNER JOIN entityindividual ei ON e.objid=ei.objid 
-WHERE e.entityname LIKE $P{name} 
-ORDER BY e.entityname 
-
-[getLookupJuridical]
-SELECT e.objid, e.entityno, e.name, e.address, e.type 
-FROM entity e 
-INNER JOIN entityjuridical ej ON e.objid=ej.objid 
-WHERE e.entityname LIKE $P{name} 
-AND ej.orgtype = $P{orgtype}
-ORDER BY e.entityname 
-
-
-[removeContacts]
-DELETE FROM entitycontact WHERE entityid=$P{objid}  
-
-[getContacts]
-SELECT * FROM entitycontact WHERE entityid=$P{objid} 
-
-[lookup]
-SELECT e.objid, e.entityno, e.name, e.address, e.type 
-FROM entity e 
-WHERE e.entityname LIKE $P{name} and type IN ('INDIVIDUAL','JURIDICAL') 
+WHERE e.entityname LIKE $P{searchtext} 
+${filter}
 ORDER BY e.entityname 
 
 [findPhoto] 
